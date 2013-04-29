99bc949703d7ff5c1f784a0f2af6f74bcc7eb10b
Per Bernhardt <plb@webfactory.de>
2013-04-29 21:34:44 +0200

    AP 1.3 Container-Configuration and Factory

---
+++ src/model/Factory.java
@@ -0,0 +1,26 @@
+package model;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+
+public class Factory {
+
+	ApplicationContext context;
+	
+	public Factory() {
+		context = new FileSystemXmlApplicationContext("spring.xml");
+	}
+	
+	public IAddressList createAddressList() {
+		return context.getBean(IAddressList.class);
+	}
+	
+	public IPostalAddress createPostalAddress() {
+		return context.getBean(IPostalAddress.class);
+	}
+	
+	public IEmailonlyAddress createEmailonlyAddress() {
+		return context.getBean(IEmailonlyAddress.class);
+	}
+	
+}
